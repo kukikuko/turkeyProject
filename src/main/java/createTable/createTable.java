820278beaae92b03e4017ecb8efd38bbd6e25ae9
hhj <gurwn805@naver.com>
2022-11-16 10:55:58 +0900

    create table 추가

--- src/main/java/createTable/createTable.java
+++ src/main/java/createTable/createTable.java
@@ -12,6 +12,7 @@ public class createTable {
 	    dept_id number
 	);
 	
+	
 	--로그인한 학생 테이블
 	create table login_user
 	(
@@ -19,7 +20,8 @@ public class createTable {
 	    user_name varchar2(128)
 	);
 	
-	--어드민 아이디, 비밀번호 테이블
+	
+	--관리자 아이디, 비밀번호 테이블
 	create table admin
 	(
 	    admin_id varchar2(128),
@@ -29,7 +31,38 @@ public class createTable {
 	values('admin', 'admin123');
 	
 	
+	-- 강의 정보 테이블
+	create table lecture_info 
+	(
+		indexId number,
+		department varchar2(128),
+		subjectNumber varchar2(128),
+		subjectName varchar2(128),
+		classTime varchar2(128),
+		lectureRoom varchar2(128),
+		professor varchar2(128)
+	);
 	
+	
+	--교수 테이블
+	create table test_prof
+	(
+		pf_name varchar2(128),
+		pf_dept varchar2(128),
+		pf_no number
+	);
+	
+	
+	--학생 테이블
+	create table test_student
+	(
+		sd_id varchar2(128),
+		sd_pw varchar2(128),
+		sd_name varchar2(128),
+		sd_email varchar2(128),
+		sd_pnvarchar2(128)
+	);
+
 */
 	
 	
