e23907ae0d1b6bb434bc25d596c766be0b38ef9e
hhj <gurwn805@naver.com>
2022-11-16 09:33:38 +0900

    admin 테이블 추가

--- src/main/java/createTable/createTable.java
+++ src/main/java/createTable/createTable.java
@@ -2,19 +2,35 @@ package createTable;
 
 public class createTable {
 
-//	create table test_user_dept 
-//	(
-//	    user_id varchar2(128),
-//	    dept_no number,
-//	    dept_id number
-//	);
+/*
+
+ 	--학생 수강신청 정보 테이블
+	create table test_user_dept 
+	(
+	    user_id varchar2(128),
+	    dept_no number,
+	    dept_id number
+	);
 	
-//	create table login_user
-//	(
-//	    user_id varchar2(128),
-//	    user_name varchar2(128)
-//	);
+	--로그인한 학생 테이블
+	create table login_user
+	(
+	    user_id varchar2(128),
+	    user_name varchar2(128)
+	);
 	
+	--어드민 아이디, 비밀번호 테이블
+	create table admin
+	(
+	    admin_id varchar2(128),
+	    admin_pw varchar2(128)
+	);
+	insert into admin
+	values('admin', 'admin123');
+	
+	
+	
+*/
 	
 	
 }
